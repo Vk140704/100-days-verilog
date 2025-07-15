f39930188c850e5c71916717b5fb0eb2edeb25f7
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-07-15 15:39:39 +0530

    Update HA_d.v

--- Half_adder db/HA_d.v
+++ Half_adder db/HA_d.v
@@ -3,3 +3,17 @@ module ha_d(input a,b,output sum,carry);
 assign sum=a&b;
 assign carry=a^b;
 endmodule
+
+//gate level modelling
+module ha_g(input a, b,output sum,carry);
+and g1(a,b,sum);
+xor g2(a,b,carry);
+endmodule
+
+//behaviour level 
+module ha_b(input a,b,output reg sum,carry);
+  always@(*)begin //this is known as sensetivity list which make update and perform whethere any value changes in port list
+sum=a&b;
+carry=a^b;
+end
+endmodule
