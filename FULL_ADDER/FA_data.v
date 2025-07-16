934faa50c01a30a078e6c508441c81fbfed7af2e
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-07-16 16:09:49 +0530

    Update FA_data.v

--- FULL_ADDER/FA_data.v
+++ FULL_ADDER/FA_data.v
@@ -1,11 +1,11 @@
 //dataflow model
 module fa(input a,b,c,output sum,carry);
-assign sum=(a&b&c);
-assign carry=(a^b^c);
+assign sum=(a^b^c);
+assign carry=(a&b&c);
 endmodule
 
 //gate level
 module fa(input a,b,c,output sum,carry);
-and g1(a,b,c,sum);
-xor g2(a,b,c,carry);
+ xor g1(a,b,c,sum);
+and g2(a,b,c,carry);
 endmodule
