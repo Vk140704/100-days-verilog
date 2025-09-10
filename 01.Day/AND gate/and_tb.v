5697f07ebadbfbffd8cdf59c7af230407580ea7e
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-10 15:27:36 +0530

    Update and_tb.v

--- 01.Day/AND gate/and_tb.v
+++ 01.Day/AND gate/and_tb.v
@@ -1,17 +1,17 @@
 module and_tb;
   reg a,b;
   wire out;
-  and uut(.a(a),.b(b),.out(out));
+  andg uut(.a(a),.b(b),.out(out));
   initial begin
   $dumpfile("and_tb.vcd");
   $dumpvars();
-  $display("  Time  |  A B  |  out  ");
+  $display("  Time  |  A B   |  out  ");
   $display("-------------------------");
-  $monitor("  %0t  |  %b %b  |  %b  ");
+  $monitor("  %0t   |  %b %b  |  %b  ",$time,a,b,out);
   a=0; b=0; #10;
   a=0; b=1; #10;
   a=1; b=0; #10;
   a=1; b=1; #10;
-  $finish
+  $finish;
   end
 endmodule
