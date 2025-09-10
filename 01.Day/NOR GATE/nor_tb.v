5ec8a39f2ec25823fb6b1a161bc71ed6d26402a6
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-10 15:59:15 +0530

    Create nor_tb.v

---
+++ 01.Day/NOR GATE/nor_tb.v
@@ -0,0 +1,17 @@
+module nor_tb;
+  reg a,b;
+  wire out;
+  norg uut(.a(a),.b(b),.out(out));
+  initial begin
+  $dumpfile("and_tb.vcd");
+  $dumpvars();
+  $display("  Time  |  A B   |  out  ");
+  $display("-------------------------");
+  $monitor("  %0t   |  %b %b  |  %b  ",$time,a,b,out);
+  a=0; b=0; #10;
+  a=0; b=1; #10;
+  a=1; b=0; #10;
+  a=1; b=1; #10;
+  $finish;
+  end
+endmodule
